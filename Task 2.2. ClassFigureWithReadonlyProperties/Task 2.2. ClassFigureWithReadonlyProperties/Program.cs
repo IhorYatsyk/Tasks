f72181a59ed2d46c3539f54a78524cc60993e110
Ihor Yatsyk <ihoryatsyk85@gmail.com>
2019-08-24 21:03:53 +0300

    Added the task 2.2.

---
+++ Task 2.2. ClassFigureWithReadonlyProperties/Task 2.2. ClassFigureWithReadonlyProperties/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task_2._1._ClassFigureWithReadonlyProperties
+{
+    public abstract class Figure
+    {
+        public abstract void Draw();
+        public readonly int X, Y;
+        public Figure()
+        {
+            X = 1;
+            Y = 0;
+        }
+    }
+    public class Square : Figure
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("I am square");
+            Console.WriteLine("Coords of square upp left angle is ({0};{1})", X, Y);
+        }
+    }
+    public class Rectangle : Figure
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("I am rectangle");
+            Console.WriteLine("Coords of rectangle upp left angle is ({0};{1})", X, Y);
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Square square = new Square();
+            square.Draw();
+            Rectangle rectangle = new Rectangle();
+            rectangle.Draw();
+            Console.ReadLine();
+        }
+    }
+}
