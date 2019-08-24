115a3d07e55f5cffdd61696014022a64cd0c0063
Ihor Yatsyk <ihoryatsyk85@gmail.com>
2019-08-24 12:43:22 +0300

    Added the task 2.1.

---
+++ Task 2.1. ClassFigureWithAbstractVoidDraw/Task 2.1. ClassFigureWithAbstractVoidDraw/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_2._1._ClassFigureWithAbstractVoidDraw
+{
+    public abstract class Figure
+    {
+        public abstract void Draw();
+    }
+    public class Square : Figure
+    {
+        public override void Draw()
+        {
+           Console.WriteLine("I am square");
+        }
+    }
+    public class Rectangle : Figure
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("I am rectangle");
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Square square = new Square();
+            square.Draw();
+            Rectangle rectangle = new Rectangle();
+            rectangle.Draw();
+            Console.ReadLine();
+        }
+    }
+}
